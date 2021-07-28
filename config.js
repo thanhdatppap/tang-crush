e6cfcdf7c0bb30c997a9685c98893c1269ec7932
thanhdatppap <88106772+thanhdatppap@users.noreply.github.com>
2021-07-29 01:07:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'bissbisss',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    desc: 'Phải chăng em đã ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yew anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: {''},
+    mess: 'Anh biết mà 🥰. Đốt bể nước em nhiều nhiều 😘😘',
+    messDesc: 'Tối mai 7h anh qua dotbenuoc nhe:3',
     btnAccept: 'Okiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
